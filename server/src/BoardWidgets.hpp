5be176a28cae88269861b1c99fe26ff590037378
ArtiTikhonov <artitikhonovrok@gmail.com>
2026-03-24 22:37:10 +0300

    BoardWidget hpp

--- server/src/BoardWidgets.hpp
+++ server/src/BoardWidgets.hpp
@@ -1,9 +1,39 @@
 #pragma once
 
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QListWidget>
+#include <QPushButton>
+#include <QLineEdit>
+
 namespace widgets {
 
-struct BaseWidget {
-    int x, y;
+class BoardsListWidget : public QWidget {
+    Q_OBJECT
+    
+public:
+    explicit BoardsListWidget(QWidget* parent = nullptr);
+    
+    void addBoard(uint64_t boardId, const QString& boardName);
+    void clear();
+    
+signals:
+    void boardSelected(uint64_t boardId);
+    void createBoardRequested(const QString& boardName);
+    
+private slots:
+    void onItemDoubleClicked(QListWidgetItem* item);
+    void onCreateClicked();
+    
+private:
+    void setupUI();
+    
+    QVBoxLayout* mainLayout;
+    QListWidget* boardsList;
+    QPushButton* createBtn;
+    QLineEdit* boardNameEdit;
 };
 
-}  // namespace widgets
+}
